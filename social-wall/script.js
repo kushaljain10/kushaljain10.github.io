fdfdd954a738f86fe72dc0e66d8adc0feb236e9c
Kushal Jain <jain.kushal10@gmail.com>
2023-11-30 17:35:36 +0530

    added social wall

---
+++ social-wall/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var iframe = document.getElementById("scrollingFrame");
+//   var style = document.createElement("style");
+//   style.textContent = `
+//     .wall-notifications {
+//         display: none;
+//     }
+//     `;
+
+//   iframe.addEventListener("load", function () {
+//     iframe.contentDocument.head.appendChild(style);
+//   });
+// });
