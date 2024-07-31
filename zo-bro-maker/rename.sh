24e616239be6b429ec25cd1f4e01b0ca273a9ebf
Kushal Jain <jain.kushal10@gmail.com>
2024-07-31 22:32:45 +0530

    updated bro maker and bae maker logic

--- zo-bro-maker/rename.sh
+++ zo-bro-maker/rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change to the directory containing the SVG files
-cd ./assets/Bros/mouth || { echo "Failed to change directory"; exit 1; }
+cd ./assets/Baes/background || { echo "Failed to change directory"; exit 1; }
 
-Rename the files using a temporary name to avoid conflicts
-for i in {1..19}; do
+# Rename the files using a temporary name to avoid conflicts
+for i in {0..20}; do
   temp_name="temp_$i.svg"
   if [ -f "$i.svg" ]; then
     sudo mv "$i.svg" "$temp_name" || { echo "Failed to rename $i.svg to $temp_name"; exit 1; }
@@ -15,8 +15,8 @@ for i in {1..19}; do
 done
 
 # Rename the temporary files to the final names
-for i in {1..19}; do
-  new_index=$((i - 1))
+for i in {0..20}; do
+  new_index=$((i + 1))
   temp_name="temp_$i.svg"
   if [ -f "$temp_name" ]; then
     sudo mv "$temp_name" "$new_index.svg" || { echo "Failed to rename $temp_name to $new_index.svg"; exit 1; }
